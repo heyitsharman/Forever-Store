4df2795f7e1ad8b6a64941a3eaf4313ae27c4efb
heyitsharman <harmankaur0779@gmail.com>
2025-09-06 08:19:49 +0530

    feat: add sorting logic and update subcategories section in ecommerce site

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -11,6 +11,7 @@ const Collection = () => {
   const [filterProducts,setFilterProducts]=useState([]);
 const [category , setCategory]= useState([]);
 const [subCategory, setSubCategory] = useState([]);
+const [sortType,setSortType] = useState('relevant');
 
 const toggleCategory = (e)=>{
   if(category.includes(e.target.value)){
@@ -36,14 +37,32 @@ const applyFilter = () =>{
   if(category.length > 0){
     productsCopy = productsCopy.filter(item => category.includes(item.category));
   }
+  if(subCategory.length > 0){
+    productsCopy = productsCopy.filter(item => subCategory.includes(item.subCategory));
+  }
   setFilterProducts(productsCopy);
 }
 
-  useEffect(() => {
-  if (Array.isArray(products)) {
-    setFilterProducts(products);
+const sortProduct = () =>{
+  let fpCopy = filterProducts.slice();
+  switch(sortType){
+    case 'low-high':
+      setFilterProducts(fpCopy.sort((a,b)=>(a.price - b.price)));
+      break;
+    case 'high-low':
+      setFilterProducts(fpCopy.sort((a,b)=>(b.price-a.price)));
+      break;
+    default:
+      applyFilter();
+      break;
+
   }
-}, [products]);
+}
+//   useEffect(() => {
+//   if (Array.isArray(products)) {
+//     setFilterProducts(products);
+//   }
+// }, [products]);
 
 // useEffect(()=>{
 //   console.log(subCategory);
@@ -53,6 +72,9 @@ useEffect(()=>{
   applyFilter();
 },[category,subCategory]);
 
+useEffect(()=>{
+  sortProduct();
+},[sortType])
 
   return (
     <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
@@ -100,7 +122,7 @@ useEffect(()=>{
       <div className='flex justify-between text-base sm:text-2xl mb-4'>
         <Title text1={'ALL'} text2={'COLLECTIONS'}></Title>
         {/* profuct Sort */}
-        <select className='border-2 border-gray-300 text-sm px-2'>
+        <select onChange={(e)=>setSortType(e.target.value)} className='border-2 border-gray-300 text-sm px-2'>
           <option value="relavent">Sort by Relavant</option>
           <option value="low-high">Sort by Low to High</option>
           <option value="high-low"> Sort by High to Low</option>

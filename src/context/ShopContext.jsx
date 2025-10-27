9fe8780f29e35e0eddef6004229d5cb84129f75d
heyitsharman <harmankaur0779@gmail.com>
2025-10-27 22:02:49 +0530

    FEAT: Login and signup functionality

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -13,6 +13,7 @@ const ShopContextProvider = (props) =>{
     const [cartItems , setCartItems]= useState({});
     const navigate = useNavigate();
     const [products,setProducts]= useState([]);
+    const [token,setToken] = useState('');
 
     const addToCart = async (itemId,size)=>{
 
@@ -107,12 +108,19 @@ useEffect(()=>{
     getProductsData();
 },[])
 
+useEffect(()=>{
+    if(!token && localStorage.getItem('token')){
+        setToken(localStorage.getItem('token'));
+    }
+},[])
+
     const value = {
         products, currency, delivery_fee,
         search,setSearch,showSearch,setShowSearch,
         cartItems, addToCart,
         getCartCount,updateQuantity,
-        getCartAmount, navigate, backendUrl
+        getCartAmount, navigate, backendUrl,
+        setToken,token
     }
 
     return (
